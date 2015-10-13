f833c7ec918ab053b7c6c1b6f02d4ab4e005a88d
fortitude.zhang <fortitude.zhang@gmail.com>
2015-10-14 00:43:25 +0800

    Fix the script which cause never load xos-ui feature. The reason is a typo in the start-karaf.sh script.

--- start-karaf.sh
+++ start-karaf.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # patch configuration file
-cp ./conf/modules.conf ./karaf/target/assembly/configuration/initial
-cp ./conf/module-shards.conf ./karaf/target/assembly/configuration/initial
+mkdir -p ./karaf/target/assembly/configuration/initial
+cp ./conf/modules.conf ./karaf/target/assembly/configuration/initial/
+cp ./conf/module-shards.conf ./karaf/target/assembly/configuration/initial/
 
 # patch org.ops4j.pax.url.mvn.repositories to use local repo.
 # note, if you adjust m2 repo, change this.
@@ -10,7 +11,8 @@ mylocalrepo=${HOME}/.m2/repository
 # check whether patched, if patched, skip the patch process.
 modified=`cat ./karaf/target/assembly/etc/org.ops4j.pax.url.mvn.cfg | grep mylocalrepo`
 if [ "$?" -ne "0" ]; then
-    sed -i  "/system\.repository/afile:${mylocalrepo}@id=mylocalrepo,\\\\" ./karaf/target/assembly/etc/org.ops4j.pax.url.mvn.cfg
+    # should enable snapshots since we currently work on dev.
+    sed -i  "/system\.repository/afile:${mylocalrepo}@id=mylocalrepo@snapshots,\\\\" ./karaf/target/assembly/etc/org.ops4j.pax.url.mvn.cfg
 fi
 
 cd ./karaf/target/assembly/bin
